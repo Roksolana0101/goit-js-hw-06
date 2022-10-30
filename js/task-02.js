1bcf034cdb13a466fd8dfd22e3b84b92c40b56bc
Roksolana0101 <107188470+Roksolana0101@users.noreply.github.com>
2022-10-30 13:52:26 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listElement = document.querySelector('#ingredients');
+const elements = ingredients.map(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    listItem.classList.add('item');
+    return listItem;
+});
+listElement.append(...elements);
