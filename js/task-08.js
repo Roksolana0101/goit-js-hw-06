f3450597b60b73c092cd7d9e0145938d05174dfe
Roksolana0101 <107188470+Roksolana0101@users.noreply.github.com>
2022-10-30 15:11:38 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const inputform = document.querySelector(".login-form");
 
+inputform .addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Потрібно заповнити всі поля форми!");
+  }
+  const formElements = { mail: email.value, password: password.value };
+
+  console.log(formElements);
+
+  event.currentTarget.reset;
+}
