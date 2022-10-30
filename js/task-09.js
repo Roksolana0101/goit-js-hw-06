0882bf4516cae869809749acbf79ea7b11fd2ebd
Roksolana0101 <107188470+Roksolana0101@users.noreply.github.com>
2022-10-30 15:22:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonColor = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+function changeButtonColor(color) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
+
+buttonColor.addEventListener("click", changeButtonColor);
