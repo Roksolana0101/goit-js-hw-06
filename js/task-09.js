5ad67d73e2f8dee8117ba772feb126fad302be40
Roksolana0101 <107188470+Roksolana0101@users.noreply.github.com>
2022-10-31 10:00:33 +0200

    correct color

--- js/task-09.js
+++ js/task-09.js
@@ -6,8 +6,14 @@ const buttonColor = document.querySelector(".change-color");
 const span = document.querySelector(".color");
 
 function changeButtonColor(color) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${randomColor}`;
+  span.textContent = `${randomColor}`;
 }
 
 buttonColor.addEventListener("click", changeButtonColor);
+// В 9 завданні у вас різні кольори в текст контенті 
+// спана та бекграунді боді, тому що ви 2 рази виклкикаєте
+//  getRandomHexColor і вона повертає 2 різні кольори.
+//Її потрібно в колбекі слухача викликати один раз, 
+// зберігати колір в змінну і потім використовувати цей колір щоб записати його в текст контент спана, та бекграунд боді
